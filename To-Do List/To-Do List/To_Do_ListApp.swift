2abe13c2baa981a0d0525aa83f0a41dc7c2d0fa3
Kevin Dam <kevindam@Kevins-MacBook-Pro-3.local>
2024-06-04 12:28:27 -0400

    setting up

---
+++ To-Do List/To-Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To-Do List
+//
+//  Created by Kevin Dam on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
