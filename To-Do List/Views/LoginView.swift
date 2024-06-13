092973dd2df735a09723b5e2c6b4af2732cb7c34
Kevin Dam <kevindam@Kevins-MacBook-Pro-3.local>
2024-06-13 01:17:14 -0400

    implemented login form

--- To-Do List/Views/LoginView.swift
+++ To-Do List/Views/LoginView.swift
@@ -8,15 +8,38 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
         VStack {
             // Header
             HeaderView()
             
             // Login Form
-            
+            Form {
+                TextField("Email Address", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                SecureField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                Button {
+                    // Attempt Log In
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(Color.blue)
+                        
+                        Text("Log In")
+                            .foregroundColor(Color.white)
+                            .bold()
+                    }
+                }
+            }
             
             // Create Account
+            
             
             Spacer()
         }
